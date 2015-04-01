08d36e9f4a33cad2b710330da004e99d00f24565
Stiffrock <christofer.malmberg@gmail.com>
2015-04-01 13:39:08 +0200

    Recover

--- TCOBO/TCOBO/Camera2D.cs
+++ TCOBO/TCOBO/Camera2D.cs
@@ -28,7 +28,7 @@ namespace TCOBO
         public void Update(GameTime gameTime)
         {
             playerPos = player.GetPos();
-            centre = new Vector2(playerPos.X-400, playerPos.Y-260);
+            centre = new Vector2(playerPos.X-320, playerPos.Y-180);
             transform = Matrix.CreateScale(new Vector3(1,1,0))*
                 Matrix.CreateTranslation(new Vector3(-centre.X, -centre.Y,0));
         }
