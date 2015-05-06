9e3a354c5830f7e753631e0bcccfff1b1e5ca677
Stiffrock <christofer.malmberg@gmail.com>
2015-05-06 18:16:30 +0200

    Merge remote-tracking branch 'origin/Stoffe' into Stoffe
    Conflicts:
    	TCOBO.v12.suo
    	TCOBO/TCOBO/Tile.cs
    	TCOBO/TCOBO/bin/x86/Debug/TCOBO.exe
    	TCOBO/TCOBO/bin/x86/Debug/TCOBO.pdb
    
    	TCOBO/TCOBO/obj/x86/Debug/ContentPipeline-{F57F5713-2EE6-4A39-821D-E7329A4EA41E}.xml
    	TCOBO/TCOBO/obj/x86/Debug/TCOBO.csproj.FileListAbsolute.txt
    	TCOBO/TCOBO/obj/x86/Debug/TCOBO.exe
    	TCOBO/TCOBO/obj/x86/Debug/TCOBO.pdb
    
    	TCOBO/TCOBOContent/obj/x86/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    
    	TCOBO/TCOBOContent/obj/x86/Debug/TCOBOContent.contentproj.FileListAbsolute.txt

--- TCOBO/TCOBO/Tile.cs
+++ TCOBO/TCOBO/Tile.cs
@@ -53,10 +53,6 @@ namespace TCOBO
             {
                 msModifier = 0.7F;        
                 collisionEnabled = true;
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/Stoffe
                 image = TextureManager.bushtile1;
 
             }
