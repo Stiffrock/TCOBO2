ea74b28afba41c5d8032788c92af5f6c0bfe366d
Hanzzon <kvarnbykillen@hotmail.com>
2015-05-08 12:30:57 +0200

    Snaptogrid och inventory fix

--- TCOBO/TCOBO/Sword.cs
+++ TCOBO/TCOBO/Sword.cs
@@ -16,30 +16,30 @@ namespace TCOBO
         //private Vector2 pos;
        
 
-<<<<<<< HEAD
         public bool Equiped;
 
         //public Stone(ContentManager content) : base(content)
-        public Sword(ContentManager content) : base(content)
-        {
-            swordTex = TextureManager.standardSword;
-            
-            //this.stonePos = new Vector2(300, 300);
-            //hitBox = new Rectangle((int)stonePos.X, (int)stonePos.Y, stoneTex.Width, stoneTex.Height);
+        //public Sword(ContentManager content)
+        //    : base(content)
+        //{
+        //    swordTex = TextureManager.standardSword;
 
-            Equiped = false;
-            this.pos = new Vector2(300, 300);
-            hitBox = new Rectangle((int)pos.X, (int)pos.Y, swordTex.Width, swordTex.Height);
-=======
+        //    //this.stonePos = new Vector2(300, 300);
+        //    //hitBox = new Rectangle((int)stonePos.X, (int)stonePos.Y, stoneTex.Width, stoneTex.Height);
+
+        //    Equiped = false;
+        //    this.pos = new Vector2(300, 300);
+        //    hitBox = new Rectangle((int)pos.X, (int)pos.Y, swordTex.Width, swordTex.Height);
+        //}
 
 
-        public Sword(ContentManager content) : base(content)
+        public Sword(ContentManager content)
+            : base(content)
         {
             this.itemTex = TextureManager.standardSword;
             this.pos = new Vector2(300, 300);
             hitBox = new Rectangle((int)pos.X, (int)pos.Y, itemTex.Width, itemTex.Height);
-            
->>>>>>> origin/Stoffe
+
         }
 
         public override void Update(GameTime gameTime)
@@ -67,7 +67,6 @@ namespace TCOBO
 
         public override void Draw(SpriteBatch sb)
         {
-<<<<<<< HEAD
             //if (!Equiped)
             //{
             //    sb.Draw(stoneTex, stonePos, Color.White);
@@ -76,11 +75,9 @@ namespace TCOBO
             //{
             //    sb.Draw(stoneTex, stonePos, Color.Red);
             //}
-            sb.Draw(swordTex, pos,new Rectangle(0,0,50,50) , Color.White);
-=======
+            //sb.Draw(swordTex, pos,new Rectangle(0,0,50,50) , Color.White);
             sb.Draw(itemTex, pos,new Rectangle(0,0,50,50) , Color.White);
 
->>>>>>> origin/Stoffe
         }
 
     }
